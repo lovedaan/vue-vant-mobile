297844063f854482793895e997e461bd4628316e
赖海涛 <897955713@qq.com>
2020-01-05 21:39:57 +0800

    提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,88 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="primary" @click="showPicker">主要按钮</van-button>
+		<transition name="slide-fade">
+			<div class="area-picker" v-if="isShow">
+				<div class="picker">
+					<van-area :area-list="areaList" @confirm="confirmPicker" @cancel="cancelPicker" />
+				</div>
+			</div>
+		</transition>
+		
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import areaList from '@/utils/area.js'
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  components: {},
+	data() {
+		return {
+			areaList,
+			areaCode: '',
+			show: false,
+			isShow: false,
+		}
+	},
+	methods: {
+		animateShow() {
+			this.show = !this.show;
+		},
+		showPicker() {
+			this.isShow = true;
+		},
+		confirmPicker(res) {
+			this.isShow = false;
+			console.log(res);
+		},
+		cancelPicker() {
+			this.isShow = false;
+		}
+	}
 }
 </script>
+<style lang="less">
+
+	.home {
+		.box {
+			position: fixed;
+			left: 0;
+			bottom: 0;
+			width: 200px;
+			height: 200px;
+			background-color: red;
+		}
+	}
+	.area-picker {
+		position: fixed;
+		left: 0;
+		top: 0;
+		z-index: 99;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5);
+		&.slide-fade-enter-active,
+		&.slide-fade-leave-active {
+			transition: all 0.5s;
+			.picker { 
+				transition: all 0.5s;
+			}
+		}
+		&.slide-fade-enter,
+		&.slide-fade-leave-to{
+		    opacity:0;
+				.picker {
+					bottom: -300px;
+				}
+		}
+		.picker {
+			position: absolute;
+			left: 0;
+			bottom: 0;
+			width: 100%;
+			background-color: #fff;
+		}
+	}
+</style>
